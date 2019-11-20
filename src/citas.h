5eab35befa1b3ffd4ea5e1697b81e61cb20c420a
i82almim <56344794+i82almim@users.noreply.github.com>
2019-11-20 17:12:16 +0100

    W

--- src/citas.h
+++ src/citas.h
@@ -3,51 +3,39 @@
 #include <string>
 using namespace std;
 
-class citas{
+class Cita{
 
 	private: 
-
 		string _fecha;
 		int _duracion;
 		string _hora;
-
-
+	
 	public:
-		citas();
-
+		Cita();
+	
 		string getFecha(){
-
 			return _fecha;
 		}
-
+	
 		int getDuracion(){
-
 			return _duracion;
 		}
 
 		string getHora(){
-
 			return _hora;
 		}
 
 		void setFecha(string fecha){
-
 			_fecha=fecha;
 		}
 
 		void setDuracion(int duracion){
-
 			_duracion=duracion;
 		}
 
 		void setHora(string hora){
-
 			_hora=hora;
 		}
-
 };
 
 #endif 
-
-
-
